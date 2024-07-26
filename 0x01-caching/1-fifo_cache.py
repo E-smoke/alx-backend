01e93ede9e2d987498455c86d160bc4457609e5f
Elijah <eachala5@gmail.com>
2024-07-26 16:29:29 +0000

    1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" BaseCaching module
+"""
+from base_caching import BaseCaching
+
+class FIFOCache(BaseCaching):
+    '''
+    ama
+    '''
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            if len(self.cache_data) < self.MAX_ITEMS:
+                self.cache_data[key] = item
+            else:
+                first_key = next(iter(self.cache_data))
+                self.cache_data.pop(first_key)
+                self.cache_data[key] = item
+                print('DISCARD: {}'.format(first_key))
+
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key:
+            return self.cache_data.get(key)
+        return None
